b9ff82cbc2f028dfbd0a3f5b323723ff0f4a920e
Byron Salty <byronsalty@gmail.com>
2023-12-01 23:13:32 -0600

    day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,32 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def part1(input_str) do
+    input_str
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      [[_, first]] = Regex.scan(~r/^\D*(\d).*$/, line)
+      [[_, last]] = Regex.scan(~r/^.*(\d)\D*$/, line)
+      first <> last
+    end)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  @digits [
+    {"one", "o1e"},
+    {"two", "t2o"},
+    {"three", "t3e"},
+    {"four", "f4r"},
+    {"five", "f5e"},
+    {"six", "s6x"},
+    {"seven", "s7n"},
+    {"eight", "e8t"},
+    {"nine", "n9e"}
+  ]
+
+  def part2(input_str) do
+    Enum.reduce(@digits, input_str, fn {word, digit}, acc ->
+      String.replace(acc, word, digit)
+    end)
+    |> part1()
   end
 end
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -4,17 +4,76 @@ defmodule AdventOfCode.Day01Test do
   import AdventOfCode.Day01
 
   @tag :skip
-  test "part1" do
-    input = nil
+  test "part1 example" do
+    input1 = """
+    12
+    1ab4
+    """
+    assert 26 == part1(input1)
+
+    input2 = """
+    12
+    asdf7asdf0fff
+    aaa1ab467
+    """
+    assert 99 == part1(input2)
+
+
+    input3 = """
+    2
+    110
+    """
+    assert 32 == part1(input3)
+
+  end
+
+  @tag :skip
+  test "part1 real" do
+    input = File.read!("inputs/input01.txt")
+
     result = part1(input)
+
+    IO.puts("Day 01 Part 1: #{result}")
 
     assert result
   end
 
+  test "part2 units" do
+    input1 = """
+    two12
+    1absix4
+    two
+    """
+    assert 58 == part2(input1)
+
+    input2 = """
+    12three
+    asdf7aeightsdf0fff
+    aaa1fourab467
+    """
+    assert 100 == part2(input2)
+
+    input3 = """
+    12threefo
+    uraaafour1fourab467nine
+    """
+    assert 62 == part2(input3)
+
+    input4 = """
+    eightwo
+    nineightwone
+    """
+    assert (82 + 91) == part2(input4)
+
+  end
+
   @tag :skip
   test "part2" do
-    input = nil
+    input = File.read!("inputs/input01.txt")
+
     result = part2(input)
+
+    IO.puts("Day 01 Part 2: #{result}")
 
     assert result
   end
